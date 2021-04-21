19c8c53919441a0c04da7ffc9d0cff6d10d3b3a8
Jayla Bullard <jaylabullard@Jaylas-Air.attlocal.net>
2021-04-21 14:45:43 -0400

    Transitions updated

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-//Appear on scroll
-const scroll = document.querySelector('#aboutme')
-window.addEventListener('scroll', onScroll)
+//AboutMe Appear on scroll
+// const scroll = document.querySelector('#aboutme')
+// window.addEventListener('scroll', onScroll)
 
-// onScroll()
+// function onScroll (){
+//     if (window.scrollY > scroll.offsetHeight + 200){
+//         scroll.classList.add('show')
+//         scroll.classList.remove('hide')
+//     }
+// }
 
-function onScroll (){
-    if (window.scrollY > scroll.offsetHeight + 200){
-        scroll.classList.add('show')
-        scroll.classList.remove('hide')
-    }
-}
+$(document).ready(function () {
+//Scroll to section
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+//Projects appear on scroll
+    $(window).on('scroll', function() {
+        const pgTop = $(document).scrollTop();
+        const pgBott = pgTop + $(window).height();
+        const projectCard = $(".project-card");
+
+        for(var i = 0; i < projectCard.length; i++) {
+            var projectCards = projectCard[i];
+
+        if($(projectCards).position().top < pgBott){
+
+            $(projectCard).addClass('visible');
+            $(projectCard).removeClass('hide');
+        }
+        }
+    })
+});
 
 // Dynamic Nav Bar
 const nav = document.querySelector('.nav-menu')
