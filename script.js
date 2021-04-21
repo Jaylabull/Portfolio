b1ed2e15ed4813bbbc9fabe5badc4f536c394edf
Jayla Bullard <jaylabullard@Jaylas-Air.attlocal.net>
2021-04-21 12:35:52 -0400

    Added dynamic scroll to aboutme section

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//Appear on scroll
+const scroll = document.querySelector('#aboutme')
+window.addEventListener('scroll', onScroll)
+
+// onScroll()
+
+function onScroll (){
+    if (window.scrollY > scroll.offsetHeight + 200){
+        scroll.classList.add('show')
+        scroll.classList.remove('hide')
+    }
+}
+
 // Dynamic Nav Bar
 const nav = document.querySelector('.nav-menu')
 window.addEventListener('scroll', fixNav)
